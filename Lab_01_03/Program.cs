cfa7dfe2165eb909e6ebea650b9c5a2b74e33b97
nexzanavod <aanavod@gmail.com>
2019-06-24 07:25:59 +0530

    first commit

---
+++ Lab_01_03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+            int no1 = Convert.ToInt32(Console.ReadLine());
+            int no2 = Convert.ToInt32(Console.ReadLine());
+
+            int temp;
+
+            temp = no1;
+            no1 = no2;
+            no2 = temp;
+
+            Console.WriteLine("After swaping numbers=\n"+no1+" \n"+no2);
+            Console.ReadLine();
+        }
+    }
+}
